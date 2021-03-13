9c2fd24f507bb4d0d4d4809a352c7b6a3a544c25
hardyyb2 <hardikb3732@gmail.com>
2021-03-13 19:33:25 +0530

    [COMPLETED] memo

--- memo/src/App.js
+++ memo/src/App.js
@@ -1,7 +1,35 @@
+import { memo, useState } from "react";
+
 import "./App.css";
 
 const App = () => {
-  return <div className="App"></div>;
+  const [header, setHeader] = useState("Header");
+  const [text, setText] = useState("Hi");
+
+  return (
+    <div className="App">
+      <div className="header">{header}</div>
+      <div className="button_container">
+        <button className="buttons" onClick={() => setHeader("New Header")}>
+          Change Header
+        </button>
+        <button className="buttons" onClick={() => setText("Bye")}>
+          Change Child Text
+        </button>
+      </div>
+
+      {Array(500)
+        .fill()
+        .map((_, index) => (
+          <ChildComponent key={index} text={text} />
+        ))}
+    </div>
+  );
 };
+
+const ChildComponent = memo(({ text }) => {
+  console.log("mounted");
+  return <div className="child_component">{text}</div>;
+});
 
 export default App;
